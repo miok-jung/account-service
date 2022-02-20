4332b9a199118b6dfb6bf6e0ce6ce2685a42353e
miok-jung <miok.dev@gmail.com>
2022-02-20 21:46:13 +0900

    Setting : Apply to react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./Login";
+import Main from "./Main";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={() => <Main />} />
+        <Route path="/login" component={() => <Login />} />
+      </Switch>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
