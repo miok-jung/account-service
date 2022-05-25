22ea1184752f9df9aeacd2ccf777ca2d58b31f5c
miok-jung <miok.dev@gmail.com>
2022-05-25 16:48:08 +0900

    feat: Add to Authentication - 어드민과 일반유저를 구분하는 인증 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Auth from 'hoc/auth';
 import LandingPage from './components/LandingPage';
 // NOTE USER
 import Login from './components/User/Login';
@@ -13,18 +14,23 @@ import Detail from 'components/Detail';
 import Edit from 'components/Edit';
 
 const App = () => {
+  // NOTE Auth(ComponentName, null) : 누구나 접근 가능
+  // Auth(ComponentName, true) : 로그인한 유저만 출입가능
+  // Auth(ComponentName, false) : 로그인한 유저는 출입불가
+  // Auth(ComponentName, true, true) : 로그인한 isAdmin이 true인 사람만 출입가능
+  // 즉 어드민 유저만 입장을 원할땐, true, true값을 넣는다.
   return (
     <Routes>
-      <Route path="/" exact element={<LandingPage />} />
-      <Route path="/income" element={<Income />} />
-      <Route path="/expense" element={<Expense />} />
+      <Route path="/" exact element={Auth(LandingPage, null)} />
+      <Route path="/income" element={Auth(Income, true)} />
+      <Route path="/expense" element={Auth(Expense, true)} />
       {/* <Route path="/transfer" element={<Transfer  />} /> */}
       {/* ANCHOR 공통 페이지 */}
-      <Route path={`/:url/upload`} element={<Upload />} />
-      <Route path={`/:url/:postNum`} element={<Detail />} />
-      <Route path={`/:url/edit/:postNum`} element={<Edit />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path={`/:url/upload`} element={Auth(Upload, true)} />
+      <Route path={`/:url/:postNum`} element={Auth(Detail, true)} />
+      <Route path={`/:url/edit/:postNum`} element={Auth(Edit, true)} />
+      <Route path="/login" element={Auth(Login, false)} />
+      <Route path="/register" element={Auth(Register, false)} />
     </Routes>
   );
 };
--- src/_actions/user_action.js
+++ src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER, REGISTER_USER } from './types';
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from './types';
 
 // 디스패치를 이용하여 파라메터를 받은 body값을 dataTosubmit에 넣는 상황
 export function loginUser(dataTosubmit) {
@@ -17,4 +17,9 @@ export function registerUser(dataTosubmit) {
     .then((res) => res.data);
 
   return { type: REGISTER_USER, payload: request };
+}
+export function auth() {
+  const request = axios.get('/api/users/auth').then((res) => res.data);
+
+  return { type: AUTH_USER, payload: request };
 }
